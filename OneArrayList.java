4e5f15d2b06b4355a71d6a86ead3de81a697764d
LeeHyunWon999 <142217859+LeeHyunWon999@users.noreply.github.com>
2024-05-14 23:13:39 +0900

    Update and rename Problem1.java to OneArrayList.java

--- OneArrayList.java
+++ OneArrayList.java
@@ -1,28 +1,28 @@
 import java.util.*;
 
-public class week13_1 {
+public class OneArrayList {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // 필요한 자료형을 넣어주세요
+        // input generic type
         ArrayList< __________ > list = new ArrayList< __________ >();
         System.out.print("Enter five integers and five doubles: ");
 
         /*
-            list 입력 코드
+            list : input code
          */
 
         sort(list);
 
         /*
-            list 출력 코드
+            list : output code
          */
     }
 
     public static void sort( __________ ) {
 
          /*
-            list 정렬 코드
+            list : sort code
          */
 
     }
